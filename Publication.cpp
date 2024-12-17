735cbc594a8b26a46d9b9661f8ca452f80761047
praska968680 <166193185+praska968680@users.noreply.github.com>
2024-12-17 19:00:24 +0530

    Update Publication.cpp

--- Publication.cpp
+++ Publication.cpp
@@ -1,51 +1,75 @@
-#include <iostream> 
-#include <fstream>  // file handling class 
+#include<iostream> 
 using namespace std; 
-class Employee 
-{   
-string name; 
-int ID; 
-double salary; 
-public: 
-void accept() 
+class publication 
 { 
-cout<<"Enter name : "; 
-getline(cin >> ws, name); 
-cout<<"Enter ID : "; 
-cin>>ID; 
-cout<<"Enter salary : "; 
-cin>>salary; 
+public: 
+string title; 
+float price; 
+}; 
+class book:public publication 
+{ 
+public: 
+int pagecount; 
+void info() 
+{ 
+cout<<"\nDetails of book publication"<<endl; 
+cout<<"Enter the title: "<<endl; 
+getline(cin,title); 
+cout<<"Enter the price: "<<endl; 
+cin>>price; 
+cout<<"Enter page count: "<<endl; 
+cin>>pagecount; 
+if(title==" " || price<=0 || pagecount<=0) 
+{ 
+title="Empty"; 
+price=0.0; 
+pagecount=0; 
+} 
 } 
 void display() 
 { 
-cout<<"Name : "<<name<<endl; 
-cout<<"ID : "<<ID<<endl; 
-cout<<"Salary : "<<salary<<endl; 
+cout<<"\nDetails of Book publication"<<endl; 
+cout<<"The title of the book publication is: "<<title<<endl; 
+cout<<"The price of the book publication is: "<<price<<endl; 
+cout<<"The page count of the book is: "<<pagecount<<endl; 
+} 
+}; 
+class tape:public publication 
+{ 
+public: 
+float play_time; 
+void details() 
+{ 
+cout<<"\nDetails of audio tape publication"<<endl; 
+cout<<"Enter the title: "<<endl; 
+cin>>title; 
+cout<<"Enter the price: "<<endl; 
+cin>>price; 
+cout<<"Enter audio play time in minutes: "<<endl; 
+cin>>play_time; 
+if(title==" " || price<=0 || play_time<=0) 
+{ 
+title="Empty"; 
+price=0.0; 
+play_time=0; 
+} 
+} 
+void display() 
+{ 
+cout<<"\nDetails of audio tape "<<endl; 
+cout<<"The title of the audio tape publication is: "<<title<<endl; 
+cout<<"The price of audio tape is: "<<price<<endl; 
+cout<<"The audio play time is: "<<play_time<<" minutes"<<endl; 
 } 
 }; 
 int main() 
 { 
-Employee o[5]; 
-fstream f; 
-int i, n; 
-f.open("demo1.txt", ios::out); 
-cout<<"Enter the number of employees you want to store : "; 
-cin>>n; 
-for(i=0; i<n; i++) 
-{ 
-cout<<"Enter the information of employee: "<<i+1<<endl; 
-o[i].accept(); 
-f.write((char*)&o[i], sizeof o[i]); 
-} 
-f.close(); 
-f.open("demo1.txt", ios::in); 
-cout<<"Information of employee is as follows"<<endl; 
-for(i=0; i<n; i++) 
-{ 
-cout<<"Employee"<<i+1<<endl; 
-f.write((char*)&o[i], sizeof o[i]); 
-o[i].display(); 
-} 
-f.close(); 
+book b; 
+tape t; 
+b.info(); 
+t.details(); 
+b.display(); 
+t.display(); 
 return 0; 
-}
+} 
+ 
